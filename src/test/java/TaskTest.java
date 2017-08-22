a624c0e4f887cb3652c358a4f84bc2fb941ed2e3
dennismuturia <dmuturia2922@gmail.com>
2017-08-22 11:42:52 +0300

    Added a clear database in task

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,8 +1,14 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.time.LocalDateTime;
+import org.sql2o.*;
 
 public class TaskTest {
+
+  @Before
+  public void setUp(){
+    DB.sql2o = new Sql2o("jdbc:postresql://localhost/5432/to_do_test", null, null)
+  };
 
   @Test
   public void Task_instantiatesCorrectly_true() {
@@ -55,6 +61,13 @@ public class TaskTest {
     Task firstTask = new Task("Mow the lawn");
     Task secondTask = new Task("Buy groceries");
     assertEquals(Task.find(secondTask.getId()), secondTask);
+  }
+  @After
+  public void tearDown(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "DELETE FROM tasks *;";
+      con.createQuery(sql).executeUpdate();
+    }
   }
 
 }
